21db47a85a40b03b31b3be733ab4697e518bdd9f
flori <lucchini.florian.et21.21@gmail.com>
2025-04-23 21:20:41 -0300

    .

--- actividades-en-clase.c
+++ actividades-en-clase.c
@@ -12,15 +12,12 @@ Si no se puede calcular el factorial del valor recibido, la función deberá dev
     printf("El factorial de %d es: %llu\n", -10, calcular_factorial(-10));
 */
 
-
-#include <stdio.h>
-
 unsigned long long calcular_factorial(int num)
 {
     int i;
     unsigned long long fact = 1;
 
-    if ( num >= 0 )
+    if (num >= 0)
     {
         for (i = 1; i <= num; i++)
         {
@@ -31,7 +28,6 @@ unsigned long long calcular_factorial(int num)
     {
         fact = 0;
     }
-
 
     return fact;
 }
@@ -70,14 +66,13 @@ int sumar_rango_incluido(int num_1, int num_2)
         suma += min;
         min++;
     }
-    
+
     return suma;
 }
 
-
 /*
 c)
-Escribir un programa que solicite el ingreso de valores, que representarán una cantidad de segundos. 
+Escribir un programa que solicite el ingreso de valores, que representarán una cantidad de segundos.
 El programa deberá informar al usuario, el equivalente en días, horas, minutos y segundos.
 Se debe validar que el valor ingresado sea entero y positivo, de lo contrario, deberá mostrarse el mensaje: “Valor ingresado inválido”.
 El ingreso de valores finaliza cuando el usuario ingrese como valor 0.
@@ -100,14 +95,12 @@ int calcular_dias(int *segundos)
     return dias;
 }
 
-
 int calcular_horas(int *segundos)
 {
     int horas = *segundos / 3600;
     *segundos %= 3600;
     return horas;
 }
-
 
 int calcular_minutos(int *segundos)
 {
@@ -119,8 +112,7 @@ int calcular_minutos(int *segundos)
 void mostrar_conversion(int ds, int hs, int mins, int segs)
 {
     printf("Resultado: Dias: %d, Horas: %d, Minutos: %d, Segundos: %d\n", ds, hs, mins, segs);
-
-} 
+}
 
 int es_negativo(int segundos)
 {
@@ -138,7 +130,7 @@ int main()
     obtener_datos(&segundos);
     while (es_distinto_de_cero(segundos))
     {
-        if (es_negativo(segundos)) 
+        if (es_negativo(segundos))
         {
             printf("Valor ingresado inválido");
         }
@@ -167,8 +159,10 @@ void imprimir_matriz(int matriz[FILAS][COLUMNAS])
     int i;
 
     printf("Matriz:\n");
-    for (i = 0; i < FILAS; i++) {
-        for (int j = 0; j < COLUMNAS; j++) {
+    for (i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < COLUMNAS; j++)
+        {
             printf("%d ", matriz[i][j]);
         }
         printf("\n");
@@ -179,8 +173,10 @@ void ingresar_valores_matriz(int matriz[FILAS][COLUMNAS])
 {
     int i;
     printf("Ingrese los vaores de la matriz.\n");
-    for (i = 0; i < FILAS; i++) {
-        for (int j = 0; j < COLUMNAS; j++) {
+    for (i = 0; i < FILAS; i++)
+    {
+        for (int j = 0; j < COLUMNAS; j++)
+        {
             printf("Ingrese el valor de la posicion (%d, %d): ", i, j);
             scanf("%d", &matriz[i][j]);
         }
@@ -222,7 +218,7 @@ int main() {
     int matriz[FILAS][COLUMNAS];
     int suma_1, suma_2;
     int i, vector[FILAS] = {0, 0, 0}, size_vector = sizeof(vector) / sizeof(int);
-    
+
     // Ingreso valores
     ingresar_valores_matriz(matriz);
 
@@ -232,7 +228,7 @@ int main() {
     suma_1 = suma_diagonal_principal(matriz);
     printf("La suma de la diagonal principal es: %d.\n", suma_1);
 
-    
+
     suma_2 = suma_diagonal_secundaria(matriz);
     printf("La suma de la diagonal principal es: %d.\n", suma_2);
 
@@ -244,3 +240,67 @@ int main() {
     return 0;
 }
 */
+
+// Mostrar el promedio de notas del curso.
+// Mostrar todas las notas superiores al promedio calculado.
+
+#include <stdio.h>
+
+#define MAX 50
+#define APROBO 4
+
+typedef int Tvec[MAX];
+
+void cargar_notas(Tvec vec, int *ML)
+{
+    int i = 0, nota;
+
+    printf("Ingrese notas ( -1 para terminar ): ");
+    scanf("%d", &nota);
+
+    while ((nota > -1) && (i < MAX))
+    {
+        vec[i] = nota;
+        i++;
+        printf("Siga ingresando notas ( -1 para terminar ): ");
+        scanf("%d", &nota);
+    }
+
+    *ML = i;
+}
+
+float calcular_promedio(Tvec vec, float ML)
+{
+    int i, suma = 0;
+
+    for(i = 0; i < ML; i++)
+    {
+        suma += vec[i];
+    }
+
+    return suma / ML;
+}
+
+void mostrar_notas_sup(Tvec vec, int ML, float promedio)
+{
+    int i;
+    for(i = 0; i < ML; i++)
+    {
+        if(vec[i] > promedio)
+            printf("La nota %d, es superior al promedio %.2f.\n", vec[i], promedio);
+    }
+}
+
+int main()
+{
+    Tvec vec;
+    int ML = 0;
+    float promedio;
+    cargar_notas(vec, &ML);
+    promedio = calcular_promedio(vec, ML);
+    printf("El promedio de las notas es %.2f.\n", promedio);
+    mostrar_notas_sup(vec, ML, promedio);
+
+
+    return 0;
+}
---
+++ guia-de-ejercicios-funciones.c
@@ -0,0 +1,157 @@
+/*
+Resuelva los siguientes ejercicios en lenguaje C.
+Para la solución de los siguientes ejercicios, no debes imprimir resultados dentro de las
+funciones que escribas. Los resultados deben ser devueltos mediante el return de la
+función.
+Importante:
+Luego de escribir cada función, probala, invocándola desde el bloque principal del
+programa, pasándole distintos valores para que la prueba contemple varias
+alternativas y así estar seguro que funciona adecuadamente.
+*/
+
+/*
+1) Escribir una función que reciba un valor n, entero, y devuelva la suma de los
+valores entre 0 y n.
+Ejemplos:
+suma_n(5) = 15
+suma_n(120) = 7260
+suma_n(120120) = 7214467260
+*/
+
+
+
+// ---------------------------------
+
+/*
+2) Escribir una función que dado un número entero, devuelva un valor booleano
+que indique si dicho número es primo o no. Antes de plantear una solución ten
+presente las siguientes preguntas.
+a) Con sólo encontrar un divisor del número a evaluar distinto a uno y a sí
+mismo, ya puedo afirmar que el número no es primo. ¿Tiene sentido seguir
+evaluando más divisores?
+b) Teniendo en cuenta que todo número par a excepción del 2, no es primo,
+¿Tiene sentido seguir en un ciclo, si al calcular el resto de la división del número
+a evaluar por 2, el resultado es cero?
+c) Puedo encontrar un divisor del número a evaluar que sea mayor al número a
+evaluar dividido 2?
+1
+Universidad de Buenos Aires Facultad de Ingeniería
+Algoritmos y Programación I Cátedra Azcurra - Guía 2025_v1
+*/
+
+
+
+// ---------------------------------
+
+/*
+3) Escribir una función que reciba un valor entero y calcule el factorial del
+mismo. Si no se puede calcular el factorial del valor recibido, la función deberá
+devolver 0, de lo contrario deberá devolver el valor calculado.
+*/
+
+
+
+// ---------------------------------
+
+/*
+4) Escribir una función que dados 2 números, calcule el porcentaje que el
+primero representa respecto del segundo.
+*/
+
+
+
+// ---------------------------------
+
+/*
+5) Escribir una función que dados a y b ambos de tipo entero devuelva a ^ b de
+tipo double para los siguientes casos (no se puede utilizar ninguna biblioteca
+de funciones matemáticas). Implementar una solución iterativa.
+En las pruebas debe invocar a la función para los siguientes 5 casos:
+i) a > 0 y b > 0
+ii) a > 0 y b < 0
+iii) a < 0 y b > 0
+iv) a < 0 y b < 0
+v) a > 0 y b = 0
+Ejemplos:
+i) a = 2 ; b = 3 ; resultado = 8
+ii) a = -2 ; b = 3 ; resultado = 8
+iii) a = -2 ; b = 0 ; resultado = 1
+iv) a = -2 ; b = 0 ; resultado = 1
+*/
+
+
+
+// ---------------------------------
+
+/*
+6) Escribir una función que dado un número entero positivo retorne un valor
+booleano que indique si el mismo es capicúa (palindromo).
+*/
+
+
+
+// ---------------------------------
+
+/*
+7) Escribir una función que dados a y b devuelva el cociente de la división
+entera, sin utilizar el operador correspondiente del lenguaje. Para esta
+resolución deben considerar la utilización de restas sucesivas.
+*/
+
+
+
+// ---------------------------------
+
+/*
+8) Escribir una función que dados a y b devuelva el resto de la división entera,
+sin utilizar el operador correspondiente de lenguaje. Para esta resolución deben
+considerar la utilización de restas sucesivas.
+2
+Universidad de Buenos Aires Facultad de Ingeniería
+Algoritmos y Programación I Cátedra Azcurra - Guía 2025_v1
+*/
+
+
+
+// ---------------------------------
+
+/*
+9) Escribir una función que dada una fecha en formato aaaammdd (número
+entero) la convierta en los parámetros numéricos aaaa, mm y dd.
+Ejemplo:
+fecha: 20140321 -> dia=21 mes=3 año=2014
+*/
+
+
+
+// ---------------------------------
+
+/*
+10) Escribir una función que dado un tiempo expresado en segundos, devuelva
+por parámetros el equivalente en días, horas, minutos y segundos.
+Utilizar esta función dentro de un programa que solicite el valor al usuario.
+Se debe validar que el valor ingresado sea entero positivo, de lo contrario,
+deberá mostrarse el mensaje: “Valor ingresado inválido”.
+Ejemplo:
+Valor ingresado representando una cantidad de segundos: 1234567
+Resultado: Días: 14, Horas: 6, Minutos: 56, Segundos 7
+Para verificar el resultado pueden ir a la siguiente web:
+https://www.satsig.net/training/seconds-days-hours-minutes-calculator.htm
+*/
+
+
+
+// ---------------------------------
+
+/*
+11) Escribir una función que dado un código numérico de m dígitos, retorne un
+sub-código consistente en una cantidad 0 < n < m de dígitos a partir del k-ésimo
+lugar a contar del dígito menos significativo.
+Los parámetros de la función deben ser código, n y k.
+En el caso que los parámetros no sean consistentes y la extracción no puede
+realizarse la función debe retornar -1.
+Si el sub-código contiene ceros a izquierda serán ignorados en el valor
+entregado como resultado.
+*/
+
+// ---------------------------------
